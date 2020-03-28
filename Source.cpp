7425348d8b5e4b05be86d836759c8033737982fb
Garaga Rayudu <40457156+Rayudu786@users.noreply.github.com>
2020-03-28 11:10:06 +0530

    Find
    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
    For example, given [10, 15, 13, 8] and k of 18, return true since 10 + 8 is 18.
    
    Bonus: Can you do this in one pass?  O(N)

---
+++ Source.cpp
@@ -0,0 +1,70 @@
+/*
+Author: Rayudu
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <unordered_map>
+
+using namespace std;
+
+class Number {
+	public:
+	Number() 
+	{
+		
+	}
+	bool isAnytwoNumber(int nArr[], int size,int k)
+	{
+		unordered_map<int,int> myMap;
+		bool results = false;
+		if (size < 2)
+			return results;
+		
+		for (int i = 0; i < size; i++)
+		{
+			if (myMap.find(nArr[i]) != myMap.end())
+			{
+				results = true;
+			}
+			else
+			{
+				myMap[k - nArr[i]] = 1;
+			}
+
+
+		}
+
+		return results;
+	}
+};
+
+int main()
+{
+	Number obj;
+	int n;
+	int k;
+	int *Arr;
+	cout << "Enter Size of Array :";
+	cin >> n;
+	Arr = new int[n*sizeof(int)];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> Arr[i];
+	}
+	cout << "Enter k Value :";
+	cin >> k;
+	cout << "Results: ";
+	cout << obj.isAnytwoNumber(Arr, n, k) << endl;
+	delete [] Arr;
+	getchar();
+	getchar();
+
+	return 0;
+}
